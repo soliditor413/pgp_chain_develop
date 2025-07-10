fdea3d9c89f49c581a0031fc5e16049310d22e7e
vito <zenith@elascript.net>
2025-07-10 17:00:01 +0800

    add test log

--- consensus/pbft/network.go
+++ consensus/pbft/network.go
@@ -844,6 +844,7 @@ func (p *Pbft) OnFailedWithdrawTxReceived(id peer.PID, msg *dmsg.FailedWithdrawT
 }
 
 func (p *Pbft) OnProducersMsg(msg *dmsg.ProducersMsg) {
+	fmt.Println(">>>>> OnProducersMsg <<<<<<", "msg ", msg.CMD())
 	currentProducers := msg.Producers
 	if msg.SpvHeight > spv.GetSpvHeight() {
 		p.needChangeNextTurnProducers = true
